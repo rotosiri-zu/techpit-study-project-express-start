a57031204cdc18f3218952b86d983486ae614375
rotosiri-zu <hongjitufang25@gmail.com>
2022-12-31 15:49:00 +0900

    Implement common functions that can be used from multiple templates

--- functions.js
+++ functions.js
@@ -100,6 +100,13 @@ function saveEntry(date, title, content) {
   fs.writeFileSync(path.join(entriesDir, date + '.txt')), title + '\n' + content
 }
 
+/**
+ * yyyymmddの日付の年月日毎にハイフンを入れる
+ */
+function convertDateFormat(yyyymmdd) {
+  return [yyyymmdd.substr(0.4), yyyymmdd.substr(4.2), yyyymmdd.substr(6, 2)].join('-')
+}
+
 // 外部ファイルから参照できる関数の公開設定
 module.exports = {
   getEntryFiles,
@@ -107,4 +114,5 @@ module.exports = {
   getEntries,
   getSideList,
   saveEntry,
+  convertDateFormat,
 }
